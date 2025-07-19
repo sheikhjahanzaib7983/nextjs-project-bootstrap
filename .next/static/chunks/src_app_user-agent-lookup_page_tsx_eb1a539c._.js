0d261c10d0d3f6192f35ea5c8208a3ec9e329afe
sheikhjahanzaib7983 <jzsheikhg@gmail.com>
2025-07-19 08:23:18 -0700

    Add .next/static/chunks/src_app_user-agent-lookup_page_tsx_eb1a539c._.js 1752945797375

---
+++ .next/static/chunks/src_app_user-agent-lookup_page_tsx_eb1a539c._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_313f1097._.js"
+],
+    source: "dynamic"
+});

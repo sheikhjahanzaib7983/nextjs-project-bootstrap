5c6b53e59affa3856c81c23be137e179f6d23372
sheikhjahanzaib7983 <jzsheikhg@gmail.com>
2025-07-19 08:22:51 -0700

    Add .next/static/chunks/src_app_generator_page_tsx_516d1a23._.js 1752945770577

---
+++ .next/static/chunks/src_app_generator_page_tsx_516d1a23._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_9a7e45bb._.js",
+  "static/chunks/node_modules_290ad755._.js"
+],
+    source: "dynamic"
+});
